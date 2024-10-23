672bea972271e62ade7111404488d2adaac967a5
Alexandre Kandalintsev <exe.sre@gmail.com>
2024-10-23 10:51:49 +0200

    minor improvements

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import gc
 import time
 
-import config
 import network
-from machine import ADC, SPI, WDT, Pin, deepsleep
+from machine import ADC, DEEPSLEEP_RESET, SPI, WDT, Pin, deepsleep, reset_cause
 
+import config
+from iot import rtc_mem
 from iot.calibration import interpolate
 from iot.ha import HA
 from iot.pcd8544_flip import LCD
 from iot.wifi import connect
 
 SPI_RATE = 2_000_000
-POLL_INTERVAL_MINS = 10
+POLL_INTERVAL_SECONDS = 600
 DRIVE = Pin.DRIVE_0
 
 
@@ -26,31 +27,18 @@ def mem_info(lcd):
 
 
 def enable_external_antenna():
-    rf_switch_power = Pin(3, mode=Pin.OUT, value=0)
-    select_external_antenna = Pin(14, mode=Pin.OUT, value=1)
-
-
-def sleep(interval, bl, rst, cs):
-    # holding pin state during sleep for the following reasons:
-    # backlight to keep backlight on
-    # rst so display is not reset when going to sleep
-    # cs so that no garbage on display screen on next wake up
-    bl.init(hold=True)
-    rst.init(hold=True)
-    cs.init(hold=True)
-
-    deepsleep(interval)
+    _rf_switch_power = Pin(3, mode=Pin.OUT, value=0)
+    _select_external_antenna = Pin(14, mode=Pin.OUT, value=1)
 
 
 def get_battery_level(adc, bat_voltage_cal, bat_percentage_cal):
     adc.read_u16()  # discard first sample just in case
 
-    num_samples = 400
+    num_samples = 200
     v_sum = 0
     for x in range(num_samples):
         _v = adc.read_u16()
         v_sum += _v
-        # time.sleep_ms(50)  # let capacitor restore charge
     adc_voltage = v_sum / num_samples
 
     bat_voltage = interpolate(bat_voltage_cal, adc_voltage)
@@ -61,17 +49,28 @@ def get_battery_level(adc, bat_voltage_cal, bat_percentage_cal):
 
 def main():
     # WATCHDOG
-    watchdog = WDT(timeout=30_000)
+    WDT(timeout=30_000)
+
+    # SET UP
+    coldboot = reset_cause() != DEEPSLEEP_RESET
+    sleep_duration = POLL_INTERVAL_SECONDS
+    backlight_num_blinks = 0
+    state = rtc_mem.recall()
+
+    if coldboot:
+        sleep_duration = 10
+        backlight_num_blinks = 5
 
     # ACTIVITY LED
-    led = Pin(15, mode=Pin.OUT, value=0)  # goes off in sleep by itself
+    Pin(15, mode=Pin.OUT, value=0)  # goes off in sleep by itself
 
     # BACKLIGHT
     print("initialize backlight")
     backlight = Pin(0, mode=Pin.OUT, drive=Pin.DRIVE_0, hold=False)
-    for x in range(5):
-        backlight.value(not backlight.value())
-        time.sleep(0.1)
+    if coldboot:
+        for x in range(backlight_num_blinks):
+            backlight.value(not backlight.value())
+            time.sleep(0.1)
     backlight.on()
 
     # LCD_SPI
@@ -87,16 +86,29 @@ def main():
     lcd = LCD(spi, cs, dc, rst, flip=True, echo=True)
     lcd.clear()
 
+    # DEEP SLEEP
+    def sleep(interval):
+        # holding pin state during sleep for the following reasons:
+        # backlight to keep backlight on
+        # rst so display is not reset when going to sleep
+        # cs so that no garbage on display screen on next wake up
+        backlight.init(hold=True)
+        rst.init(hold=True)
+        cs.init(hold=True)
+
+        deepsleep(interval)
+
     # ADC
     adc = ADC(1, atten=ADC.ATTN_0DB)
-    time.sleep(1)
+    if coldboot:
+        lcd.log("cold delay")
+        time.sleep(10)
     bat_percentage, bat_voltage = get_battery_level(
         adc,
         config.BAT_VOLTAGE_CAL,
         config.BAT_PERCENTAGE_CAL,
     )
-    lcd.log(f"bat: {bat_percentage}%")
-    lcd.log(f"bat: {bat_voltage:.2f}V")
+    lcd.log(f"B:{bat_percentage}% {bat_voltage:.2f}V")
 
     bat_charged = True
     if bat_percentage < 20:
@@ -111,6 +123,11 @@ def main():
         lcd.log(f"(sleeping {sleep_dur_minutes}m)")
         sleep(sleep_dur_minutes * 60 * 1000, bl=backlight, rst=rst, cs=cs)
 
+    # RECALL PREVIOUS STATE
+    if state:
+        lcd.log("old state:")
+        lcd.log(f"{state['temp']:.2f}C")
+        lcd.log(f"{state['ts']}")
     # WIFI
     enable_external_antenna()
     lcd.log("connecting")
@@ -125,14 +142,14 @@ def main():
         #         print(RSSI)
         # lcd.log(f"RSSI: {RSSI}")
 
-        ip = connect(
+        connect(
             wlan,
             hostname=config.HOSTNAME,
             netcreds=config.WIFI_CREDS,
         )
-    except Exception as err:
+    except Exception as _err:
         lcd.log("wifi fiasco")
-        sleep(5_000, bl=backlight, rst=rst, cs=cs)
+        sleep(10_000)
 
     # HA
     lcd.log("home ass.")
@@ -147,22 +164,25 @@ def main():
 
     # MAIN SCREEN
     lcd.clear()
-    lcd.log(f"{temperature}C")
+    lcd.log(f"{temperature:.2f}C")
     lcd.log("")
     lcd.log(f"{time_now}")
     lcd.log(f"{bat_percentage}% {bat_voltage:.2f}")
+
+    # STORE CURRENT STATE
+    rtc_mem.store({"temp": temperature, "ts": time_now})
 
     # DEINIT
     wlan.disconnect()
     wlan.active(False)
 
-    # DEEP SLEEP
+    # GO TO DEEP SLEEP
     if bat_charged:
-        lcd.log(f"Zzz {POLL_INTERVAL_MINS}min")
+        lcd.log(f"Zzz {sleep_duration//60}min")
     else:
         lcd.log("PLZ CHARGE!")
 
-    sleep(POLL_INTERVAL_MINS * 60 * 1000, bl=backlight, rst=rst, cs=cs)
+    sleep(sleep_duration * 1000)
 
 
 if __name__ == "__main__":
